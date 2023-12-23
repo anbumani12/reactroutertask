9930631c32b371e7ebdaccce7d7eafc9de384fe2
anbumani12 <anbumani15012002@gmail.com>
2023-12-23 22:25:06 +0530

    Task completed

---
+++ src/components/All.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+
+function All() {
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Scalable-Vector-Graphics-SVG-for-Responsive-UI-Design.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Scalable Vector Graphics (SVG) for Responsive UI Design
+              </h5>
+              <p className="card-text">
+                Scalable Vector Graphics (SVG) has revolutionized the world of
+                graphics and web design.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">23 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Top-Ways-To-Use-AI-in-UX-Design.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">Top 7 Ways To Use AI in UX Design</h5>
+              <p className="card-text">
+                Artificial Intelligence (AI) has revolutionized various
+                industries, and the field of User Experience (UX) design is no
+                exception.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">22 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-A-Guide-to-Master-UIUX-Design-With-Online-Courses.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                A Guide to Master UI/UX Design With Online Courses
+              </h5>
+              <p className="card-text">
+                In the evolving world of digitalization, the synthesis of user
+                interface and user experience is the driving force behind
+                companies.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">21 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Best-UIUX-Prototyping-Tools.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">8 Best UI/UX Prototyping Tools</h5>
+              <p className="card-text">
+                If you’re into designing websites or apps, you know how
+                important it is to create a sample (or prototype) of your work
+                before it’s finished
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">23 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/How-to-create-a-UI-UX-design-portfolio.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                How to Create a UI UX Design Portfolio: Top 3 Essential Tips
+              </h5>
+              <p className="card-text">
+                Creating your first UI UX design portfolio is a monumental step
+                in your design career.{" "}
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">22 December 2023</small>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-UX-Designer-Resume-Examples-and-Portfolio-Guide.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                8 UX Designer Resume Examples and Portfolio Guide
+              </h5>
+              <p className="card-text">
+                The journey to landing your dream job as a UX designer begins
+                with a powerful tool: your resume.{" "}
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">21 December 2023</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default All;
---
+++ src/components/CyberSecurity.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+
+function CyberSecurity() {
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Non-Coding-Jobs-in-Cybersecurity.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Non-Coding Jobs in Cybersecurity: A Comprehensive Guide
+              </h5>
+              <p className="card-text">
+                In the rapidly evolving field of cybersecurity, there is a
+                common misconception that coding skills.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">04 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                What Is Hacking? Types of Hacking & More
+              </h5>
+              <p className="card-text">
+                Have you ever wondered what hacking is all about? It’s a big
+                deal in today’s tech-heavy world.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">25 September 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2022/12/Yellow-Blue-Illustration-Business-Blog-Banner-2048x1152.png"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Cybersecurity Vs Ethical Hacking: Top 10 Differences
+              </h5>
+              <p className="card-text">
+                Cybersecurity & Ethical hacking and have been key in making sure
+                that your data online is secure data and privacy concerns are all kept safe.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">27 December 2022</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2022/12/cybersecuity-.gif"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                What is Cybersecurity? Importance and its uses & the growing
+                challenges in 2023!
+              </h5>
+              <p className="card-text">
+                Look around today, you will witness that we are more reliant on
+                technology and devices.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">20 December 2022</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2022/11/Teal-Illustration-Digital-Business-Blog-Banner-1-2048x1152.png"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                8 Different Types of Cybersecurity and Threats Involved
+              </h5>
+              <p className="card-text">
+                Cybersecurity refers to the protection of devices, processes,
+                infrastructure, cyberattacks, data theft, breaches, unauthorized access, etc.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">09 November 2022</small>
+            </div>
+          </div>
+        </div>
+          <div className="col">
+            <div className="card h-100">
+              <img
+                src="https://www.guvi.in/blog/wp-content/uploads/2022/06/8bfd01c18be1b5059bc0d7770d9dabf1.gif"
+                className="card-img-top"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  Is coding required for cybersecurity? If yes, how crucial is
+                  coding for cybersecurity?
+                </h5>
+                <p className="card-text">
+                  Many people ask how important is coding for cybersecurity, and
+                  the lawyerly answer is: Well, it depends on many facets.
+                  <span className="read-more">
+                    <strong>READ MORE</strong>
+                  </span>
+                </p>
+              </div>
+              <div className="card-footer">
+                <small className="text-body-secondary">22 June 2022</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default CyberSecurity;
