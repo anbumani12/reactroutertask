e0f04ae8b0fea9f8eff384332139af3f546b8e5d
anbumani12 <anbumani15012002@gmail.com>
2023-12-28 11:50:04 +0530

    image updated

--- src/components/CyberSecurity.jsx
+++ src/components/CyberSecurity.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function CyberSecurity() {
   return (
@@ -57,24 +57,26 @@ function CyberSecurity() {
         <div className="col">
           <div className="card h-100">
             <img
-              src="https://www.guvi.in/blog/wp-content/uploads/2022/12/Yellow-Blue-Illustration-Business-Blog-Banner-2048x1152.png"
+              src="https://www.guvi.in/blog/wp-content/uploads/2021/03/Blog-9-2048x1072.png"
               className="card-img-top"
               alt=""
             />
             <div className="card-body">
               <h5 className="card-title">
-                Cybersecurity Vs Ethical Hacking: Top 10 Differences
+                The Cybersecurity Surge: 5 Must-Have Cybersecurity
+                Certifications!
               </h5>
               <p className="card-text">
-                Cybersecurity & Ethical hacking and have been key in making sure
-                that your data online is secure data and privacy concerns are all kept safe.
+                There is something fascinating about a lone hacker in black
+                hoodies using a single system and their skills to take down big
+                corporations and their crony capitalism.
                 <span className="read-more">
                   <strong>READ MORE</strong>
                 </span>
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-body-secondary">27 December 2022</small>
+              <small className="text-body-secondary">01 March 2021</small>
             </div>
           </div>
         </div>
@@ -82,25 +84,27 @@ function CyberSecurity() {
         <div className="col">
           <div className="card h-100">
             <img
-              src="https://www.guvi.in/blog/wp-content/uploads/2022/12/cybersecuity-.gif"
+              src="https://www.guvi.in/blog/wp-content/uploads/2022/06/Types-of-Cyber-Security-Attacks-and-How-to-Minimize-the-Impact.jpg"
               className="card-img-top"
               alt=""
             />
             <div className="card-body">
               <h5 className="card-title">
-                What is Cybersecurity? Importance and its uses & the growing
-                challenges in 2023!
+                Top 7 Cyber Security Attacks in Real Life
               </h5>
               <p className="card-text">
-                Look around today, you will witness that we are more reliant on
-                technology and devices.
+                Cyber security attacks are the type of actions that are designed
+                to destroy, steal, modify, or disable information through
+                unauthorized access to computer systems. Globally, India in 2021
+                was among the top three nations that experienced the most server
+                access & ransomware attacks.
                 <span className="read-more">
                   <strong>READ MORE</strong>
                 </span>
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-body-secondary">20 December 2022</small>
+              <small className="text-body-secondary">01 March 2021</small>
             </div>
           </div>
         </div>
@@ -108,54 +112,56 @@ function CyberSecurity() {
         <div className="col">
           <div className="card h-100">
             <img
-              src="https://www.guvi.in/blog/wp-content/uploads/2022/11/Teal-Illustration-Digital-Business-Blog-Banner-1-2048x1152.png"
+              src="https://www.guvi.in/blog/wp-content/uploads/2021/03/The-Ultimate-%E2%80%A8Cybersecurity-Roadmap-for-Beginners.webp"
               className="card-img-top"
               alt=""
             />
             <div className="card-body">
               <h5 className="card-title">
-                8 Different Types of Cybersecurity and Threats Involved
+                The Ultimate Cybersecurity Roadmap for Beginners
               </h5>
               <p className="card-text">
-                Cybersecurity refers to the protection of devices, processes,
-                infrastructure, cyberattacks, data theft, breaches, unauthorized access, etc.
+                Cybersecurity jobs are also one of the most handsomely paying
+                jobs in recent times. Furthermore, the annual growth rate of
+                cybersecurity jobs as identified by LinkedIn is 30%.
                 <span className="read-more">
                   <strong>READ MORE</strong>
                 </span>
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-body-secondary">09 November 2022</small>
+              <small className="text-body-secondary">01 March 2021</small>
             </div>
           </div>
         </div>
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src="https://www.guvi.in/blog/wp-content/uploads/2022/06/8bfd01c18be1b5059bc0d7770d9dabf1.gif"
-                className="card-img-top"
-                alt=""
-              />
-              <div className="card-body">
-                <h5 className="card-title">
-                  Is coding required for cybersecurity? If yes, how crucial is
-                  coding for cybersecurity?
-                </h5>
-                <p className="card-text">
-                  Many people ask how important is coding for cybersecurity, and
-                  the lawyerly answer is: Well, it depends on many facets.
-                  <span className="read-more">
-                    <strong>READ MORE</strong>
-                  </span>
-                </p>
-              </div>
-              <div className="card-footer">
-                <small className="text-body-secondary">22 June 2022</small>
-              </div>
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2020/11/cyber-security-e1614342903477.png"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                How Is Cyber Security Important To Our Lives?
+              </h5>
+              <p className="card-text">
+                Cybersecurity is an exact solution that is sought either by a
+                billionaire with a massive business or an innovator with a small
+                start-up or even an individual who risks personal data online.
+                Let’s see more.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">10 November 2020</small>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
