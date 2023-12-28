426a0858da687ec774c2c17160707eec985f836d
anbumani12 <anbumani15012002@gmail.com>
2023-12-28 12:00:14 +0530

    task completed

--- src/components/CyberSecurity.jsx
+++ src/components/CyberSecurity.jsx
@@ -57,61 +57,6 @@ function CyberSecurity() {
         <div className="col">
           <div className="card h-100">
             <img
-              src="https://www.guvi.in/blog/wp-content/uploads/2021/03/Blog-9-2048x1072.png"
-              className="card-img-top"
-              alt=""
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                The Cybersecurity Surge: 5 Must-Have Cybersecurity
-                Certifications!
-              </h5>
-              <p className="card-text">
-                There is something fascinating about a lone hacker in black
-                hoodies using a single system and their skills to take down big
-                corporations and their crony capitalism.
-                <span className="read-more">
-                  <strong>READ MORE</strong>
-                </span>
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-body-secondary">01 March 2021</small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="card h-100">
-            <img
-              src="https://www.guvi.in/blog/wp-content/uploads/2022/06/Types-of-Cyber-Security-Attacks-and-How-to-Minimize-the-Impact.jpg"
-              className="card-img-top"
-              alt=""
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                Top 7 Cyber Security Attacks in Real Life
-              </h5>
-              <p className="card-text">
-                Cyber security attacks are the type of actions that are designed
-                to destroy, steal, modify, or disable information through
-                unauthorized access to computer systems. Globally, India in 2021
-                was among the top three nations that experienced the most server
-                access & ransomware attacks.
-                <span className="read-more">
-                  <strong>READ MORE</strong>
-                </span>
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-body-secondary">01 March 2021</small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="card h-100">
-            <img
               src="https://www.guvi.in/blog/wp-content/uploads/2021/03/The-Ultimate-%E2%80%A8Cybersecurity-Roadmap-for-Beginners.webp"
               className="card-img-top"
               alt=""
@@ -131,30 +76,6 @@ function CyberSecurity() {
             </div>
             <div className="card-footer">
               <small className="text-body-secondary">01 March 2021</small>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card h-100">
-            <img
-              src="https://www.guvi.in/blog/wp-content/uploads/2020/11/cyber-security-e1614342903477.png"
-              className="card-img-top"
-              alt=""
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                How Is Cyber Security Important To Our Lives?
-              </h5>
-              <p className="card-text">
-                Cybersecurity is an exact solution that is sought either by a
-                billionaire with a massive business.
-                <span className="read-more">
-                  <strong>READ MORE</strong>
-                </span>
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-body-secondary">10 November 2020</small>
             </div>
           </div>
         </div>
